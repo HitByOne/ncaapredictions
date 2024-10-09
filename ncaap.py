1f6463f86fe1cdb8f7699309ef6e105fa8109bba
HitByOne <156109007+HitByOne@users.noreply.github.com>
2024-10-09 18:38:41 -0500

    Update ncaap.py

--- ncaap.py
+++ ncaap.py
@@ -4,29 +4,27 @@ import pandas as pd
 # Set Streamlit to wide mode
 st.set_page_config(layout="wide")
 
-# Load the CSV file from your local directory
-file_path = '/Users/ybkmykeyz/Desktop/NFL Info/ncaa_predictions.csv'
-data = pd.read_csv(file_path)
-
 # Correct variable name for the NCAA sheet ID
 ncaa_sheet_id = '19_lirG8bfTvccuBbFxfeCuRywJA0LZY1jt7_umjUCRA'
+
+# Load data from Google Sheets directly
 ncaa_predictions = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{ncaa_sheet_id}/export?format=csv")
 
 # Create a new column combining 'Away' and 'Home' teams
-data['Away @ Home'] = data['Away'] + ' @ ' + data['Home']
+ncaa_predictions['Away @ Home'] = ncaa_predictions['Away'] + ' @ ' + ncaa_predictions['Home']
 
 # Display the title
 st.title('NCAA Predictions')
 
 # Create a filter for the combined "Away @ Home", including an "All Games" option
-game_options = ['All Games'] + list(data['Away @ Home'].unique())
+game_options = ['All Games'] + list(ncaa_predictions['Away @ Home'].unique())
 selected_game = st.selectbox('Select Game (Away @ Home)', game_options)
 
 # Filter the data based on the selected game or show all if "All Games" is selected
 if selected_game == 'All Games':
-    filtered_data = data
+    filtered_data = ncaa_predictions
 else:
-    filtered_data = data[data['Away @ Home'] == selected_game]
+    filtered_data = ncaa_predictions[ncaa_predictions['Away @ Home'] == selected_game]
 
 # Display the filtered data
 st.dataframe(filtered_data)
